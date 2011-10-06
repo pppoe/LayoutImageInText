39bb22901a11c95335469495ac261831a3819bd1
mr.pppoe <mr.pppoe@gmail.com>
2011-10-06 11:09:38 +0800

    Issue Fix

--- Classes/LayoutImageInTextAppDelegate.m
+++ Classes/LayoutImageInTextAppDelegate.m
@@ -20,18 +20,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
 
-    NSString *inputPath = @"/Users/hli/Temp/input.txt";
-    NSString *outputPath = @"/Users/hli/Temp/output.txt";
+    NSString *inputText = @"乔布斯是美国硅谷的一个传奇，他是苹果公司的创始人，但是之后因为公司的内部争斗而离开了苹果公司。1997年苹果公司是风雨飘摇当时几乎有倒闭的危险，在这个时候乔布斯回到了苹果公司，开始了10多年的传奇生涯。他担任CEO之后推出了很多革命性的产品，包括iPod、iPhone和iPad，这些也为我们中国消费者所熟悉。尽管每一次都有很多人质疑，但是却引领了智能手机和平板电脑的风潮，也令苹果牢牢占据这两块市场的半壁江山。";
+//    NSString *outputPath = @"/Users/hli/Temp/output.txt";
     
-    FixWidthText *fixWidthText = [[[FixWidthText alloc] init] autorelease];
-    NSString *inputText = [NSString stringWithContentsOfFile:inputPath
-                                                    encoding:NSUTF8StringEncoding
-                                                       error:nil];
-    NSString *outputText = [fixWidthText textWithFixedWidth:inputText];
-    [outputText writeToFile:outputPath 
-                 atomically:YES
-                   encoding:NSUTF8StringEncoding
-                      error:nil];
+//    FixWidthText *fixWidthText = [[[FixWidthText alloc] init] autorelease];
+//    NSString *inputText = [NSString stringWithContentsOfFile:inputPath
+//                                                    encoding:NSUTF8StringEncoding
+//                                                       error:nil];
+//    NSString *outputText = [fixWidthText textWithFixedWidth:inputText];
+//    [outputText writeToFile:outputPath 
+//                 atomically:YES
+//                   encoding:NSUTF8StringEncoding
+//                      error:nil];
     
     TestView *testView = [[TestView alloc] initWithFrame:CGRectMake(10, 60, 300, 200)];
     testView.text = inputText;
